6506bc82de6181b656bddbee15f56ab19a4bef45
Diego <web.diego01@gmail.com>
2017-02-17 14:35:55 -0200

    adicionou map android api na aplicação;

--- app/src/main/java/tac/com/appandroidtac/MainActivity.java
+++ app/src/main/java/tac/com/appandroidtac/MainActivity.java
@@ -1,13 +1,27 @@
 package tac.com.appandroidtac;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btEntrar = (Button) findViewById(R.id.btEntrar);
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
