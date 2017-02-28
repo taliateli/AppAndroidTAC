58e09e728394a71453e5b8c6c1e079b645a3eb34
Diego <web.diego01@gmail.com>
2017-02-28 03:44:28 -0300

    validou permissão de localização e adicionou zoom no googleMaps;

--- app/src/main/java/tac/com/appandroidtac/MapsActivity.java
+++ app/src/main/java/tac/com/appandroidtac/MapsActivity.java
@@ -1,7 +1,12 @@
 package tac.com.appandroidtac;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.Manifest;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -38,9 +43,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng gyn = new LatLng(-16.6596276, -49.2826741);
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.addMarker(new MarkerOptions().position(gyn).title("Estamos em Goiânia"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(gyn));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
     }
 }
